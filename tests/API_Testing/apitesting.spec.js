ecaf7bd4e8b0e25706e10a2300c24e640f9bc095
Ashwin-ash22 <ashwinkumar007ash@gmail.com>
2025-06-04 20:58:18 +0530

    Api File

---
+++ tests/API_Testing/apitesting.spec.js
@@ -0,0 +1,23 @@
+import { test, expect, request } from '@playwright/test';
+
+test.only('API GET Request @ash', async ({ request }) => {
+
+  const responce = await request.get('https://reqres.in/api/users?page=2');
+  expect(responce.status()).toBe(200);
+  const text = await responce.text();
+  expect(text).toContain('Lindsay');
+  console.log(await responce.json());
+});
+
+test.only('API login @ash', async ({ page }) => {
+  await page.goto('/');
+  const loginpayload = { email: "zoro@yopmail.com", password: "Zoro@22" };
+  const apicontext = await request.newContext();
+  const loginresponce = await apicontext.post("https://sso.teachable.com/secure/9521/identity/login/password?force=true",
+    {
+      data: loginpayload
+    })
+  expect(loginresponce.ok()).toBeTruthy();
+});
+
+
